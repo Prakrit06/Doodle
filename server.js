2c888070439bca1ee4b06c12c4de11c58aab3446
Prakrit06 <70273329+Prakrit06@users.noreply.github.com>
2022-04-23 23:45:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,99 @@
+// importing
+
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+
+import Pusher from "pusher";
+import cors from "cors";
+import e from "express";
+// app config
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+// const Pusher = require("pusher");
+
+const pusher = new Pusher({
+    appId: "1367465",
+    key: "4479fa385d2aee4f4846",
+    secret: "64c945c1afac10ed95ab",
+    cluster: "ap2",
+    useTLS: true
+});
+
+// pusher.trigger("my-channel", "my-event", {
+//   message: "hello world"
+// });
+
+// middlewares
+
+app.use(express.json());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Cotrol-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "*");
+//     next();
+// });
+// DB config
+const connection_url = 'mongodb+srv://Prakrit:Pass1234@cluster0.qiiu0.mongodb.net/whatsappdb?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url); //, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log("DB connected");
+
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch()
+
+    changeStream.on("change", (change) => {
+        console.log("A Change occured", change);
+
+        if (change.operationType === "insert") {
+            const messageDetails = change.fullDocument;
+            pusher.trigger("messages", "inserted", {
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received,
+            });
+        } else {
+            console.log("Error triggerng Pusher");
+        }
+
+    });
+});
+
+// api routes
+// app.get("/", (req, res) => res.status(200).send('hello world'));
+
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    });
+});
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    });
+});
+// listener
+app.listen(port, () => console.log('Listening on localhost:${port}'));
